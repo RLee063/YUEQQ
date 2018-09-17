01fe7aaf72b143078d8cc0cf1a290bab4335f01a
firekingy <972023182@qq.com>
2018-09-17 20:09:08 +0800

    修改下拉刷新返回数据格式

--- server/controllers/pullRefresh.js
+++ server/controllers/pullRefresh.js
@@ -1,10 +1,31 @@
-const { mysql } = require('../qcloud')
-module.exports = async ctx => {
-  try{
-    var ans = await mysql.select().table('ActivitiesInfo')
-    ctx.body = ans
-  }
-  catch(e){
+const {mysql} = require('../qcloud')
+
+function formatTime(time){
+  return time.toLocaleString()
+}
+
+module.exports = async(ctx) => {
+
+  try {
+      
+    var activities = await mysql('ActivityInfo as info').join('ActivityPic as pic', 'info.Aid', 'pic.Aid').join('UserAvatar as user', 'user.uid', 'info.creatorUid').select().orderBy('StartTime','desc').limit(10)
+
+    for(var i in activities){
+      console.log(activities[i])
+      activities[i]['startTime'] = formatTime(activities[i]['startTime'])
+      activities[i]['createTime'] = formatTime(activities[i]['createTime'])
+    }
+
+    ctx.body = {
+      code: 1,
+      data: activities
+    }
+    console.log(activities)
+  } catch (e) {
     console.log(e)
-  }
+    ctx.body = {
+      code : -1,
+      data: 'failed' + e.sqlMessage
+    }
+    }
 }
