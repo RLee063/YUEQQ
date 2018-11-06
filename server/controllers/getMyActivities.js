5243ab90079aa364d9449e4d32c61660bd2a13cb
firekingy <972023182@qq.com>
2018-11-06 18:58:59 +0800

    返回的个人活动信息分为开始与未开始

--- server/controllers/getMyActivities.js
+++ server/controllers/getMyActivities.js
@@ -1,11 +1,16 @@
 const { mysql } = require('../qcloud')
-
+var tool = require('./tool.js')
 module.exports = async(ctx) => {
   const {uid} = ctx.query
+  var createdActivities = { started: [], notStart: [] }
+  var joinedActivities = { started: [], notStart: [] }
+
   try{
-    createdActivities = await mysql('ActivityInfo').select().where('creatorUid', uid)
+    createdActivities['started'] = await mysql('ActivityInfo').select().where('creatorUid', uid).andWhereRaw('StartTime <= ?', [tool.getNowFormatDate()])
+    createdActivities['notStart'] = await mysql('ActivityInfo').select().where('creatorUid', uid).andWhereRaw('StartTime > ?', [tool.getNowFormatDate()])
     joinedAids = await mysql('userAct').select('aid').where('uid', uid)
-    joinedActivities = await mysql('ActivityInfo').select().where('aid', 'in', joinedAids)
+    joinedActivities['started'] = await mysql('ActivityInfo').select().whereRaw('StartTime <= ?', [tool.getNowFormatDate()]).andWhere('aid', 'in', joinedAids)
+    joinedActivities['notStart'] = await mysql('ActivityInfo').select().whereRaw('StartTime <= ?', [tool.getNowFormatDate()]).andWhere('aid', 'in', joinedAids)
     
     ctx.body = {
       'code': 1,
